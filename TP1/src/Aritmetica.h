fd24e71ad277f84f7d01b66dabf8b834bfdf78f0
JoseEnriquez884 <bishop015@hotmail.com>
2021-04-14 22:06:12 -0300

    termine documentacion

--- TP1/src/Aritmetica.h
+++ TP1/src/Aritmetica.h
@@ -8,10 +8,49 @@
 #ifndef ARITMETICA_H_
 #define ARITMETICA_H_
 
+
+/** \brief Calcula la suma de 2 numeros flotantes
+ *
+ * \param float primer operando flotante
+ * \param float segundo operando flotante
+ * \return float resultado de la suma de 2 numeros flotantes
+ *
+ */
 float SumarNumeros(float,float);
+
+/** \brief Calcula la resta de 2 numeros flotantes
+ *
+ * \param float primer operando flotante
+ * \param float segundo operando flotante
+ * \return float resultado de la resta de 2 numeros flotantes
+ *
+ */
 float RestarNumeros(float,float);
+
+/** \brief Calcula la multiplicacion de 2 numeros flotantes
+ *
+ * \param float primer operando flotante
+ * \param float segundo operando flotante
+ * \return float resultado de la multiplicacion de 2 numeros flotantes
+ *
+ */
 float MultiplicarNumeros(float,float);
+
+/** \brief Calcula la division de 2 numeros flotantes
+ *
+ * \param float primer operando flotante (dividendo)
+ * \param float segundo operando flotante (divisor)
+ * \return float resultado de la division de 2 numeros flotantes
+ *
+ */
 float DividirNumeros(float,float);
+
+/** \brief Calcula el factorial de un numero entero
+ *
+ * \param int numero entero del cual se calculara el factorial
+ * \return long int devuelve el resultado del factorial del numero entero
+ *
+ */
 long int CalcularFactorial(int);
 
 
--- TP1/src/PedirDato.h
+++ TP1/src/PedirDato.h
@@ -9,7 +9,28 @@
 #define PEDIRDATO_H_
 
 
+/** \brief consigue un numero entero
+ *
+ * \param char[] cadena para mostrarle al usuario
+ * \param char[] cadena con mensaje de error para que el usuario reingrese un numero entero valido
+ * \param int numero minimo que se va a poder ingresar
+ * \param int numero maximo que se va a poder ingresar
+ * \return int devuelve el entero que ingreso el usuario
+ *
+ */
 int GetInt(char[],char[],int,int);
+
+
+/** \brief consigue un numero flotante
+ *
+ * \param char[] cadena para mostrarle al usuario
+ * \param char[] cadena con mensaje de error para que el usuario reingrese un numero entero valido
+ * \param int numero minimo que se va a poder ingresar
+ * \param int numero maximo que se va a poder ingresar
+ * \return float devuelve el numero flotante que ingreso el usuario
+ *
+ */
 float GetFloat(char[],char[],int,int);
+
 
 #endif /* PEDIRDATO_H_ */
