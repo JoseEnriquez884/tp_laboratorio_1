cd95c91fcc283f22f31fb0b104aae9703114da3d
JoseEnriquez884 <bishop015@hotmail.com>
2021-05-11 21:01:22 -0300

    empezando tp2

---
+++ TP2/src/TP2.c
@@ -0,0 +1,21 @@
+/*
+ ============================================================================
+ Name        : TP2.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	setbuf(stdout,NULL);
+
+	printf("esto funciona");
+
+	return EXIT_SUCCESS;
+}
